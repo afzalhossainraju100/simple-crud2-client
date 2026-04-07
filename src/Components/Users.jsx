3af00e67247e5e9dc818031c648a2c37ab368abb
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2026-04-07 18:09:51 +0600

    user added successfully

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -9,6 +9,24 @@ const handleSubmit = (e) => {
     const name = form.name.value;
     const email = form.email.value;
     console.log(name, email);
+    const newUser = { name, email };
+
+    //save this users data t database using fetch
+    fetch("http://localhost:3000/users",{
+        method:'POST',
+        headers:{
+            'content-type':'application/json'
+        },
+        body:JSON.stringify(newUser)
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("after saving the user", data);
+        if(data.insertedId){
+            alert("User added successfully");
+            e.target.reset();
+        }
+      });
 }
 
     return (
