049cddf31ef4c4a84b57dd0bfc9e4062eeb3c63a
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2026-04-07 18:23:54 +0600

    fetch data from database and console it

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import './App.css'
 import Users from './Components/Users'
 
+
+const usersPromise = fetch("http://localhost:3000/users")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log("users data", data);
+    return data;
+  })
+  .catch((error) => {
+    console.error("Error fetching users:", error);
+    return [];
+  });
+
 function App() {
 
 
   return (
     <>
       <h1>CRUD Client</h1>
-      <Users></Users>
+      <Users usersPromise={usersPromise}></Users>
     </>
-  )
+  );
 }
 
 export default App
--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { use } from 'react';
 
-const Users = () => {
-
-
-const handleSubmit = (e) => {
+const Users = ({ usersPromise }) => {
+    const initialUsers = use(usersPromise);
+    console.log("initial users", initialUsers);
+  const handleSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
     const name = form.name.value;
@@ -12,35 +13,40 @@ const handleSubmit = (e) => {
     const newUser = { name, email };
 
     //save this users data t database using fetch
-    fetch("http://localhost:3000/users",{
-        method:'POST',
-        headers:{
-            'content-type':'application/json'
-        },
-        body:JSON.stringify(newUser)
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newUser),
     })
       .then((res) => res.json())
       .then((data) => {
         console.log("after saving the user", data);
-        if(data.insertedId){
-            alert("User added successfully");
-            e.target.reset();
+        if (data.insertedId) {
+          alert("User added successfully");
+          e.target.reset();
         }
       });
-}
+  };
 
-    return (
-        <div>
-            <h1>Simple CRUD</h1>
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" name='name' placeholder="Enter user name" id='' />
-                    <input type="email" name='email' placeholder="Enter user email" id='' />
-                    <button type='submit'>Add User</button>
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Simple CRUD</h1>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="name" placeholder="Enter user name" id="" />
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter user email"
+            id=""
+          />
+          <button type="submit">Add User</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Users;
